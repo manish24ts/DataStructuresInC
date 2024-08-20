b8fbb4d957ffd31cb53a145299366fca3d267b6d
manish24ts <126053896+manish24ts@users.noreply.github.com>
2024-08-20 12:22:04 +0530

    Update Circular_single_linked_list.cpp

--- Data Structures in C/Linked List/Circular_single_linked_list.cpp
+++ Data Structures in C/Linked List/Circular_single_linked_list.cpp
@@ -1,12 +1,11 @@
 //Circular Single or Singly linked list
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<stdio.h>
-#include<stdlib.h>
-
-struct node{
-	int data;
-	struct node *next; //Self referential Structure
-}*head=NULL,*last,*n,*temp,*p;
+struct node {
+    int data;
+    struct node *next;
+} *head = NULL, *last, *n, *temp, *p;
 
 void create();
 void display();
@@ -18,170 +17,193 @@ void deleteBegin();
 void deleteEnd();
 void deleteKey();
 
-
-int main()
-{
-	int ch;
-	printf("1.Create\n2.Display\n3.Insert start\n4.Insert end\n5.Insert After key\n6.Insert Before key\n7.Delete start\n8.Delete end\n9.Delete key\n10.Exit\n");
-	do
-	{
-		printf("Enter your choice:\n");
-		scanf("%d",&ch);
-		switch(ch)
-		{
-			case 1: create();break;
-			case 2: display();break;
-			case 3: insertBegin();break;
-			case 4: insertEnd();break;
-			case 5: insertAfterKey();break;
-			case 6: insertBeforeKey();break;
-			case 7: deleteBegin();break;
-			case 8: deleteEnd();break;
-			case 9: deleteKey();break;
-			case 10: printf("Exiting the process\n");exit(1);
-			default: printf("Wrong option\n");
-		}
-	}while(ch<=10);
+int main() {
+    int ch;
+    printf("1.Create\n2.Display\n3.Insert start\n4.Insert end\n5.Insert After key\n6.Insert Before key\n7.Delete start\n8.Delete end\n9.Delete key\n10.Exit\n");
+    do {
+        printf("Enter your choice:\n");
+        scanf("%d", &ch);
+        switch (ch) {
+            case 1: create(); break;
+            case 2: display(); break;
+            case 3: insertBegin(); break;
+            case 4: insertEnd(); break;
+            case 5: insertAfterKey(); break;
+            case 6: insertBeforeKey(); break;
+            case 7: deleteBegin(); break;
+            case 8: deleteEnd(); break;
+            case 9: deleteKey(); break;
+            case 10: printf("Exiting the process\n"); exit(0);
+            default: printf("Wrong option\n");
+        }
+    } while (ch <= 10);
 }
 
-void create()
-{
-	n = (struct node*)malloc(sizeof(struct node)); //Initallising a node
-	printf("Enter data value:\n");
-	scanf("%d",&n->data);
-	n->next=NULL;
-	if(head==NULL) //If head is Null, assign n to head and last
-	{
-		head = n;
-		last = n;
-	}
-	else
-	{
-		last->next=n; //new node and last node linking
-		last=n;       // re-assignning last
-	}
-	last->next=head;
+void create() {
+    n = (struct node *)malloc(sizeof(struct node));
+    printf("Enter data value:\n");
+    scanf("%d", &n->data);
+    if (head == NULL) {
+        head = n;
+        last = n;
+        n->next = head;
+    } else {
+        last->next = n;
+        last = n;
+        last->next = head;
+    }
 }
 
-void display()
-{
-	temp = head;
-	if(temp==NULL)
-	printf("Empty list\n");
-	else
-	{
-		printf("The linked list is:\n");
-		do
-		{
-			printf("%d->",temp->data);
-			temp=temp->next;
-		}while(temp!=head);
-		printf("\n");
-	}
+void display() {
+    if (head == NULL) {
+        printf("Empty list\n");
+        return;
+    }
+    temp = head;
+    printf("The linked list is:\n");
+    do {
+        printf("%d->", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+    printf("\n");
 }
 
-void insertBegin()
-{
-	n = (struct node*)malloc(sizeof(struct node)); //Initallising a node
-	printf("Enter data value:\n");
-	scanf("%d",&n->data);
-	n->next=head;  //new node and head node linking
-	head = n;      //re-assiging head
-	last->next=head;
+void insertBegin() {
+    n = (struct node *)malloc(sizeof(struct node));
+    printf("Enter data value:\n");
+    scanf("%d", &n->data);
+    if (head == NULL) {
+        head = n;
+        last = n;
+        n->next = head;
+    } else {
+        n->next = head;
+        head = n;
+        last->next = head;
+    }
 }
 
-void insertEnd()
-{
-	n = (struct node*)malloc(sizeof(struct node)); //Initallising a node
-	printf("Enter data value:\n");
-	scanf("%d",&n->data);
-	n->next=NULL;
-	last->next = n;  //new node and last node linking
-	last = n;        //re-assiging last
-	last->next=head;
+void insertEnd() {
+    n = (struct node *)malloc(sizeof(struct node));
+    printf("Enter data value:\n");
+    scanf("%d", &n->data);
+    if (head == NULL) {
+        head = n;
+        last = n;
+        n->next = head;
+    } else {
+        last->next = n;
+        last = n;
+        last->next = head;
+    }
 }
 
-void insertAfterKey()
-{
-	int key;
-	n = (struct node*)malloc(sizeof(struct node)); //Initallising a node
-	printf("Enter data value:\n");
-	scanf("%d",&n->data);
-	printf("Enter key:\n");
-	scanf("%d",&key);
-	temp = head;
-	while(temp!=NULL) //Traversing to find key
-	{
-		if(temp->data==key)
-		break;
-		temp = temp->next;
-	}
-	n->next=temp->next;
-	temp->next=n;
+void insertAfterKey() {
+    int key;
+    printf("Enter key:\n");
+    scanf("%d", &key);
+    temp = head;
+    do {
+        if (temp->data == key) {
+            n = (struct node *)malloc(sizeof(struct node));
+            printf("Enter data value:\n");
+            scanf("%d", &n->data);
+            n->next = temp->next;
+            temp->next = n;
+            if (temp == last) {
+                last = n;
+            }
+            return;
+        }
+        temp = temp->next;
+    } while (temp != head);
+    printf("Key not found\n");
 }
 
-void insertBeforeKey()
-{
-	int key;
-	n = (struct node*)malloc(sizeof(struct node)); //Initallising a node
-	printf("Enter data value:\n");
-	scanf("%d",&n->data);
-	printf("Enter key:\n");
-	scanf("%d",&key);
-	temp = head;
-	while(temp!=NULL) //Traversing to find key
-	{
-		if(temp->data==key)
-		break;
-		else
-		{
-			p = temp;
-			temp = temp->next;
-		}
-	}
-	n->next=p->next;
-	p->next=n;
+void insertBeforeKey() {
+    int key;
+    printf("Enter key:\n");
+    scanf("%d", &key);
+    temp = head;
+    if (head->data == key) {
+        insertBegin();
+        return;
+    }
+    do {
+        p = temp;
+        temp = temp->next;
+        if (temp->data == key) {
+            n = (struct node *)malloc(sizeof(struct node));
+            printf("Enter data value:\n");
+            scanf("%d", &n->data);
+            p->next = n;
+            n->next = temp;
+            return;
+        }
+    } while (temp != head);
+    printf("Key not found\n");
 }
 
-void deleteBegin()
-{
-	temp = head;
-	head = head->next;
-	free(temp);
-	last->next=head;
+void deleteBegin() {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    temp = head;
+    if (head == last) {
+        head = NULL;
+        last = NULL;
+    } else {
+        head = head->next;
+        last->next = head;
+    }
+    free(temp);
 }
 
-void deleteEnd()
-{
-	temp = head;
-	while(temp!=last)
-	{
-		p = temp;
-		temp = temp->next;
-	}
-	p->next = NULL;
-	last = p;
-	free(temp);
-	last->next=head;
+void deleteEnd() {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    if (head == last) {
+        free(head);
+        head = NULL;
+        last = NULL;
+        return;
+    }
+    temp = head;
+    while (temp->next != last) {
+        temp = temp->next;
+    }
+    free(last);
+    last = temp;
+    last->next = head;
 }
 
-void deleteKey()
-{
-	int key;
-	temp = head;
-	printf("Enter key:\n");
-	scanf("%d",&key);
-	while(temp!=NULL)
-	{
-		if(temp->data==key)
-		break;
-		else
-		{
-			p = temp;
-			temp = temp->next;
-		}
-	}
-	p->next=temp->next;
-	temp->next=NULL;
-	free(temp);
+void deleteKey() {
+    int key;
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    printf("Enter key:\n");
+    scanf("%d", &key);
+    temp = head;
+    if (head->data == key) {
+        deleteBegin();
+        return;
+    }
+    do {
+        p = temp;
+        temp = temp->next;
+        if (temp->data == key) {
+            p->next = temp->next;
+            if (temp == last) {
+                last = p;
+            }
+            free(temp);
+            return;
+        }
+    } while (temp != head);
+    printf("Key not found\n");
 }
